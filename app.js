f61864496368a01834deb0a56a53cd1da6555b1a
Anton Voylenko <anton.voylenko@gmail.com>
2023-03-25 00:06:27 +0200

    update auth server

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const jsonParser = bodyParser.json();
-const app = express();
 const config = require('config');
 const router = require('./lib/router');
 
-const listenPort = config.get('server.port') ;
-const path = config.has('server.path') ? config.get('server.path') : '/';
+const app = express();
+app.use(express.json());
+
+const listenPort = config.get('server.port');
+const listenPath = config.get('server.path');
+
+console.log(`http auth server config: ${JSON.stringify(config)}`);
 
 app.listen(listenPort, () => {
   console.log(`http auth server listening on ${listenPort}`);
@@ -18,4 +20,4 @@ if (config.has('server.auth')) {
   app.use(basicAuth(config.get('server.auth')));
 }
 
-app.post(path, jsonParser, router) ;
+app.post(listenPath, router);
